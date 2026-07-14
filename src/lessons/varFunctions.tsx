6e1939c1fb439db72aca745ef61b6a08bb12f353
Ian <tevesiankhristopher@gmail.com>
2026-07-14 20:22:40 +0800

    day 1: variables and functions

---
+++ src/lessons/varFunctions.tsx
@@ -0,0 +1,22 @@
+export default function Day1VariablesFunctions() {
+    const name = "John";
+    let age = 21;
+
+    age = 22;
+
+    return(
+        <div>
+            <h1>Variables</h1>
+
+            <p>Name: {name}</p>
+            <p>Age: {age}</p>
+        </div>
+    )
+
+    // return (
+    //     <div>
+    //         <h1>Day 1: Variables & Functions</h1>
+    //         <p>Hello, {name}!</p>
+    //     </div>
+    // );
+}
