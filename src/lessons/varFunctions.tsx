9e58c504a09709d0408cee444c2ee17e91381b25
Ian <tevesiankhristopher@gmail.com>
2026-07-14 20:48:02 +0800

    day 1: variables and functions arrow function

--- src/lessons/varFunctions.tsx
+++ src/lessons/varFunctions.tsx
@@ -1,22 +1,55 @@
 export default function Day1VariablesFunctions() {
-    const name = "John";
+    // const name = "John";
     let age = 21;
 
-    age = 22;
+
+     const message = `Hello, my name is ${name} and I'm ${age} years old`;
+     const sentence = `Hello, I'm learning react`;
+
+     const greeting = () =>{
+        return "Welcome to react!";
+     }
+
+     const introduce = (name) => {
+        return `Hello I'm ${name} and currently learning react`;
+     }
+
+     const add = (a, b) =>{
+
+        return `${a} + ${b} = ${a + b}`;
+     }
+
+     const multiply = (x, y) =>{
+        return `${x} x ${y} = ${x * y}`;
+     }
+
+     const fullName = () =>{
+        
+     }
 
     return(
         <div>
             <h1>Variables</h1>
 
-            <p>Name: {name}</p>
-            <p>Age: {age}</p>
+            {/* <p>
+                {message}
+            </p> */}
+            <p>{message}</p>
+            <p>{sentence}</p>
+            {/* <p>Name: {name}</p>
+            <p>Age: {age}</p> */}
+
+            <hr />
+            {/* arrow functions */}
+
+            <p>{greeting()}</p>
+            <p>{introduce("ian")}</p>
+            <p>add: {add(5,3)}</p>
+            <p>multiply: {multiply(5,3)}</p>
         </div>
+        
+
+       
     )
 
-    // return (
-    //     <div>
-    //         <h1>Day 1: Variables & Functions</h1>
-    //         <p>Hello, {name}!</p>
-    //     </div>
-    // );
 }
