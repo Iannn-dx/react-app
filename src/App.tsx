2cfba59ac6be5556dc45cd48b8f8728e90874f3c
Ian <tevesiankhristopher@gmail.com>
2026-07-16 20:55:15 +0800

    day 3: objects, destructuring, spread operator

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Day1VariablesFunctions from "./lessons/varFunctions";
 import Day2VariablesFunctions from "./lessons/arrayFunctions";
+import Day3VariablesFunctions from "./lessons/objects";
 
 function App() {
     return (
         <div>
             {/* <Day1VariablesFunctions /> */}
-            <Day2VariablesFunctions />
+            {/* <Day2VariablesFunctions /> */}
+                <Day3VariablesFunctions />
         </div>
     );
 }
---
+++ src/lessons/objects.tsx
@@ -0,0 +1,46 @@
+export default function day3onject(){
+
+  const user = {
+        id: 1,
+        name: "Ian",
+        age: 21,
+        course: "BSIT"
+    };
+
+    const {name, age, course} = user;
+
+    const product = {
+      prod: "Laptop",
+      price: 42500,
+      brand: "MSI"
+    };
+
+    const {prod, price, brand} = product;
+
+    const {
+      name: username
+    } = user;
+
+    const student = {
+      name: "John",
+      section: "A"
+    };
+
+  return (
+    <div>
+      <h1>Objects, Destructuring & Spread Operator</h1>
+
+      <p>{user["name"]}</p>
+      <p>{username}</p>
+      <p>{age}</p>
+      <p>{course}</p>
+
+      <div>
+        <p>{prod}</p>
+        <p>{price}</p>
+        <p>{brand}</p>
+        <p>{prod}</p>
+      </div>
+    </div>
+  )
+}
