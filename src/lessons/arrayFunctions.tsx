762e1b41f85fdcc21c4431282fa7ea79dbe01301
Ian <tevesiankhristopher@gmail.com>
2026-07-16 20:25:50 +0800

    day 3: includes, filter, some

--- src/lessons/arrayFunctions.tsx
+++ src/lessons/arrayFunctions.tsx
@@ -27,7 +27,62 @@ export default function Day2Arrays() {
         return numbers.filter(numbers => numbers % 2 == 0);
       }
 
+      const words = [
+        {
+          id:1,
+          word: "hello"
+        },
+        {
+          id:2,
+          word: "world"
+        },
+        {
+          id:3,
+          word: "kind"
+        },
+        {
+          id:4,
+          word: "polite"
+        },
+        {
+          id:5,
+          word: "kind"
+        },
+      ];
 
+      const lang = [
+        {
+          id: 1,
+          language: "python"
+        },
+        {
+          id: 2,
+          language: "Javascript"
+        },
+        {
+          id: 3,
+          language: "PHP"
+        },
+        {
+          id: 4,
+          language: "Java"
+        },
+        {
+          id: 5,
+          language: "Kotlin"
+        },
+        {
+          id: 6,
+          language: "Typescript"
+        },
+      ];
+      // const sample = () => ()
+
+      // const language = () => {
+      //   return lang.includes(lang => lang.language === "python");
+      // }
+
+      // array of objects uses some, find, filter while includes return a value like bool
 
     // map
     // users.map(users => (
@@ -52,6 +107,16 @@ export default function Day2Arrays() {
            <p>{evenNum().map(numbers => (
             <p key={numbers}>{numbers}</p>
            ))}</p>
+
+           <div>
+            {words.filter(words => words.word == "kind").map(words => (
+              <p key={words.id}>{words.word}</p>
+            ))}
+           </div>
+
+           <div>
+            <p>{lang.includes({language:"python"})}</p>
+           </div>
         </div>
     );
 }
