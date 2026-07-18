9f4b5b861b1eb3ccd5d7a64f99b7c5d1834ae5c9
Ian <tevesiankhristopher@gmail.com>
2026-07-18 20:31:18 +0800

    Day 5: Modules & Async JavaScript

--- src/lessons/day5.tsx
+++ src/lessons/day5.tsx
@@ -1,9 +1,21 @@
+import { add, subtract, multiply, divide } from "../utils/math";
+import welcome, {goodluck} from "../utils/messages";
+
 export default function day5 () {
+
+
   return(
     <div>
       <h1>
         Modules & Async JavaScript
       </h1>
+        <p>{add(10, 5)}</p>
+        <p>{subtract(20, 8)}</p>
+        <p>{multiply(4, 6)}</p>
+        <p>{divide(300,60)}</p>
+
+        <hr />
+          <p>{welcome()}</p><p>{goodluck()}</p>
     </div>
   );
 }
---
+++ src/utils/messages.js
@@ -0,0 +1,7 @@
+const welcome = () => {
+    return "Welcome to React!";
+};
+
+export const goodluck = () => `Goodluck to learning react`;
+
+export default welcome;
