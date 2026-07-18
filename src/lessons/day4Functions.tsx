058666d6a6f74f118819b328e06fa97228f366d3
Ian <tevesiankhristopher@gmail.com>
2026-07-18 19:44:51 +0800

    continuation of day 4

--- src/lessons/day4Functions.tsx
+++ src/lessons/day4Functions.tsx
@@ -8,10 +8,38 @@ export default function day4Function(){
       return number * number;
     }
 
+    const students = [
+    {
+        id:1,
+        name:"Ian",
+        score:95
+    },
+    {
+        id:2,
+        name:"John",
+        score:75
+    },
+    {
+        id:3,
+        name:"Alice",
+        score:82
+    }
+];
+
+  const add = function (a,b) {
+    return `${a} + ${b} = ${a + b}`;
+  }
+
   return ( 
       <div>
         <p>{greet("Ian")}</p>
         <p>Square of 8 is {square(8)}</p>
+        <p>{add(8,3)}</p>
+
+        <div>
+          <hr />
+
+        </div>
       </div>
   );
 }
